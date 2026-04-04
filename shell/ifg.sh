451939fe05fd61f3f707a9ba45cade0def1e6738
GJ Tiquia <gjtiquia@gmail.com>
2026-04-05 02:22:01 +0800

    pass args to shell integration

--- shell/ifg.sh
+++ shell/ifg.sh
@@ -1,12 +1,21 @@
-# ifg - interactive command finder
+# ifg - [i] [f]or[g]ot
 # Add to ~/.bashrc OR ~/.zshrc:
 #   eval "$(ifg --sh)"
 
 ifg() {
+	if [[ $# -gt 0 ]]; then
+		command ifg "$@"
+		return $?
+	fi
+
 	local cmd=$(command ifg)
 	if [[ -n "$cmd" ]]; then
 		history -s "$cmd"
-		echo "Command: $cmd"
-		echo "Press UP to access from history"
+		echo "# ifg - [i] [f]or[g]ot"
+		echo ""
+		echo "  $cmd"
+		echo ""
+		echo "[press UP to access from history]"
+		echo ""
 	fi
 }
